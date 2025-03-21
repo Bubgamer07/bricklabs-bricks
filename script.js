6b583e878a425982d0d38097b9118f1e2b8534ff
Bubgamer07 <148393011+Bubgamer07@users.noreply.github.com>
2025-03-20 20:40:35 -0700

    Create script.js
    tabel

---
+++ script.js
@@ -0,0 +1,20 @@
+const response = await fetch("recipes.json")
+if (!response.ok) {
+  document.write("Couldn't fetch data :(")
+  throw new Error(`Error Code: ${response.status}`)
+} else {
+  const data = response.json()
+  const table = document.getElementById("table")
+  for (const recipe of data) {
+    const row = document.createElement("tr")
+    const cell1 = document.createElement("td")
+    cell1.innerText = recipe[0]
+    row.appendChild(cell1)
+    const cell2 = document.createElement("td")
+    cell2.innerText = recipe[1]
+    row.appendChild(cell2)
+    const cell3 = document.createElement("td")
+    cell3.innerText = recipe[2]
+    row.appendChild(cell3)
+  }
+}
